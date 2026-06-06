cb7fd9ced1ebfa5f8ffd8c4cbd29507e6e17d291
krimmmmm <krimmmmm@gmail.com>
2026-06-07 05:27:14 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,7 @@
 export default function App() {
   return (
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-        background: "black",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        color: "cyan",
-        fontSize: "40px",
-        fontWeight: "bold"
-      }}
-    >
-      NETJER ONLINE
-    </div>
+    <h1 style={{ color: "red", background: "white", height: "100vh" }}>
+      NETJER ONLINE TEST
+    </h1>
   );
 }
